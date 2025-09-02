3745cc03830103a3046fa7d6f27f69580feeaaa8
Fionnlagh Jones <fionnlaghjones@gmail.com>
2025-09-02 13:30:09 -0400

    initial method for getting auth token

--- controller.js
+++ controller.js
@@ -1,1 +1,16 @@
+$(document).ready(function () {
+  $("#identify").click(identifyController);
+  $("#auth").click(getAuthToken);
+});
 
+identifyController = function () {
+  alert("identify button clicked");
+};
+
+getAuthToken = function () {
+  const endpoint = "/api/v1/new";
+  let controllerAddress = $("#controllers").find(":selected").val();
+  $.post(`${controllerAddress}${endpoint}`, {}).done(function (data) {
+    console.log("Data Loaded: ", data);
+  });
+};
